531d14e0fc6c1ccdfa549e21e233aca71f99e34a
juin925 <101376856+juin925@users.noreply.github.com>
2023-06-08 20:37:51 +0900

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,13 @@
+import random
+
+person = []
+sky = []
+
+for i in range(0,5):
+    a = random.randint(0,6)
+    b = random.randint(0,6)
+    person.append(a)
+    sky.append(b)
+
+print(person)
+print(sky)
